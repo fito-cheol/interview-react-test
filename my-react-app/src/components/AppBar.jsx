2d22b1277200e21c643adc805a51018855c69097
LAPTOP-TQFM3LTV\dmsgh <dmsghs92@gmail.com>
2025-06-18 12:01:29 +0900

    appbar 선택 효과

--- my-react-app/src/components/AppBar.jsx
+++ my-react-app/src/components/AppBar.jsx
@@ -1,45 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const AppBar = () => (
-	<nav
-		style={{
-			position: 'fixed',
-			top: 0,
-			left: 0,
-			width: '100%',
-			background: '#1976d2',
-			color: 'white',
-			padding: '1rem',
-			zIndex: 1000,
-			display: 'flex',
-			justifyContent: 'center',
-			boxShadow: '0 2px 8px rgba(0,0,0,0.08)',
-		}}
-	>
-		<Link
-			to="/shoppingCart/test"
+const AppBar = () => {
+	const location = useLocation();
+
+	return (
+		<nav
 			style={{
-				marginRight: '1rem',
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: '100%',
+				background: '#1976d2',
 				color: 'white',
-				textDecoration: 'none',
-				fontWeight: 500,
+				padding: '1rem',
+				zIndex: 1000,
+				display: 'flex',
+				justifyContent: 'center',
+				boxShadow: '0 2px 8px rgba(0,0,0,0.08)',
 			}}
 		>
-			쇼핑카트 테스트
-		</Link>
-		<Link
-			to="/shoppingCart/example"
-			style={{
-				marginRight: '1rem',
-				color: 'white',
-				textDecoration: 'none',
-				fontWeight: 500,
-			}}
-		>
-			쇼핑카트 예시
-		</Link>
-		{/* <Link
+			<Link
+				to="/shoppingCart/test"
+				style={{
+					marginRight: '1rem',
+					color: location.pathname == '/shoppingCart/test' ? 'white' : 'black',
+					textDecoration: 'none',
+					fontSize: location.pathname == '/shoppingCart/test' ? 20 : 18,
+					fontWeight: location.pathname == '/shoppingCart/test' ? 900 : 500,
+				}}
+			>
+				쇼핑카트 테스트
+			</Link>
+			<Link
+				to="/shoppingCart/example"
+				style={{
+					marginRight: '1rem',
+					color:
+						location.pathname == '/shoppingCart/example' ? 'white' : 'black',
+					textDecoration: 'none',
+					fontSize: location.pathname == '/shoppingCart/example' ? 20 : 18,
+					fontWeight: location.pathname == '/shoppingCart/example' ? 900 : 500,
+				}}
+			>
+				쇼핑카트 예시
+			</Link>
+			{/* <Link
 			to="/loading"
 			style={{
 				marginRight: '1rem',
@@ -50,7 +56,8 @@ const AppBar = () => (
 		>
 			로딩바
 		</Link> */}
-	</nav>
-);
+		</nav>
+	);
+};
 
 export default AppBar;
